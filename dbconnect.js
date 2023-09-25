6759901a545dcdcc9daf4a62e74175ab39e48c8f
lhs <2hansoul@naver.con>
2023-09-25 02:34:39 +0000

    Feat:db연결

---
+++ dbconnect.js
@@ -0,0 +1,35 @@
+const mysql = require('mysql2');
+
+const connection  = mysql.createConnection({
+  host : '3.37.132.55',
+  port : '3306',
+  user : 'hansol',
+  password : '',
+  database : 'nodedata'
+});
+
+
+// 데이터베이스에 연결합니다.
+connection.connect((err) => {
+    if (err) {
+      console.error('데이터베이스 연결 오류: ' + err.stack);
+      return;
+    }
+    console.log('데이터베이스에 연결되었습니다.');
+  });
+
+ connection.query('select * from test', function(error,results,fields) {
+
+    if (error) throw error;
+    console.log('id',results);
+ });;
+
+
+ connection.end();
+  
+  
+  
+  
+  
+
+
--- server.js
+++ server.js
@@ -1,50 +1,22 @@
 
-
+/*
 const express = require('express');
 const app = express();
-
-
-app.get("/", (req,res) => {
-
-  res.send("root page");
-
-});
-
-
-app.get("/user/:id", (req,res) => {
-  const q = req.params
-  console.log(q);
-
-  res.json({'userid' : q.id});
-
-});
-
-app.get("/sound/:name", (req,res) => {
-  const { name } = req.params
-
-  console.log(name);
-
-  if(name == "dog"){
-    res.json({'sound' : 'warwar'});
-  }else if (name == "cat"){
-    res.json({'sound' : 'mimi'});
-  }
-
-  
-
-});
 
 
 
 app.listen(8080, function(){
     console.log('listening on 8080');
 });
+*/
 
-/*
 
+
+
+const { assert } = require('console');
 const http = require('http');
 
-const hostname = '127.0.0.1'; // ·ÎÄÃ È£½ºÆ®
+const hostname = '127.0.0.1'; // ë¡œì»¬ í˜¸ìŠ¤íŠ¸
 
 const port = 8080;
 
@@ -52,15 +24,24 @@ const server = http.createServer((req, res) => {
 
   res.statusCode = 200;
 
-  res.setHeader('Content-Type', 'text/plain');
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+  
 
-  res.end('Hello, World111111111111!\n');
+  res.get("/dbconnect.js" ,(req,res) => {
+    res
+  })
+
+  
 
 });
+
 
 server.listen(port, hostname, () => {
 
   console.log(`Server is running at https://${hostname}:${port}/`);
 
 });
-*/
+
+
+
+
