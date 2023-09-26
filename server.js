deafd026bfedca3c784abb402651cc47123daa7b
2hansoul <2hansoul@naver.com>
2023-09-26 11:00:13 +0000

    학습2일차 : 데이터 넘기기 get,post body

--- server.js
+++ server.js
@@ -1,20 +1,89 @@
 const express = require('express');
-const router_test = require('./routes/router');
-const join = require('./routes/join_act');
 const app = express();
-const ejs = require('ejs');
+const bodyParser = require('body-parser');
+//const ejs = require('ejs');
 
-
-//ejs 사용하기 위함
-/*
+//ejs확장자 사용하기 위해 선언
 app.set('view engine','ejs');
 app.set('views','./views');
-*/
+
+app.use(express.static('public')); //정적인 파일이 이미지
+app.use(bodyParser.urlencoded({extended :false})); // 폼 데이터 post방식 사용위해 미들웨어설정
+
+//루트페이지 
+app.get('/' ,  function(req,res){
+
+  var move_page = `<a href ="/form">form</a>`;
+    
+  res.send(move_page);
+});
+
+//url이 form 이면 form.ejs로 이동 
+app.get('/form' ,  function(req,res){
+  res.render('form');
+});
+
+//form 에서 post 방식으로 데이터 전송  body
+app.post('/form_post' ,  function(req,res){
+  var title = req.body.title;
+  var textare = req.body.textare;
+  res.send(title+','+textare);
+});
 
 
-//app.use(require('./routes/router'));
-app.use(router_test);
-//app.use('/join_act' , join);
+//form 에서 get 방식으로 데이터 전송  query
+app.get('/form_get' ,  function(req,res){
+  var title = req.query.title;
+  var textare = req.query.textare;
+  res.send(title);
+});
+
+app.get('/topic/:id' ,  function(req,res){
+  var topics = ['javascript i', 'nodejs', 'express'];
+
+  var output =`<a href="topic/0">javascript i</a><br>
+           <a href="topic/1">nodejs</a><br>
+           <a href="topic/2">express</a><br>
+           ${topics[req.params.id]}`;  //params 시멘트url 방식 query는 쿼리스트링 방식
+
+  res.send(output);
+});
+
+app.get ('/topic/:id/:mode' ,function(req,res){
+    res.send(req.params.id+','+req.params.mode);
+});
+
+
+app.get('/dynamic' ,  function(req,res){
+  var lis ='';
+  for (var i=0; i<5; i++){
+    lis = lis +'<li>coding</li>';
+  }
+  var time = Date();
+  var output = `<!DOCTYPE html>
+                <html lang="en">
+                  <head>
+                      <meta charset="UTF-8">
+                      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+                      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                      <title>Node.js Test</title>
+                  </head>
+                  <body>
+                      <div>
+                         <h2>aaaaa</h2>
+                         <ul>
+                         ${lis}
+                         </ul>
+                         ${time}
+                        <div>
+                  </body>
+                </html>`
+  res.send(output);
+});
+
+app.get('/route' ,  function(req,res){
+  res.send('hello router <img src="/imgfile.png">');
+});
 
 
 
@@ -24,22 +93,4 @@ app.listen(8080, function(){
 
 
 
-/*
-const http = require('http');
-
-const hostname = '127.0.0.1'; // 로컬 호스트
-
-const port = 8080;
-
-const server = http.createServer((req, res) => {
-
-  
-});
-
-server.listen(port, hostname, () => {
-
-  console.log(`Server is running at https://${hostname}:${port}/`);
-
-});
-*/
 
