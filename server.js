3480fc585f33b1e82f68fcdeaeca1fbf9b2ba7f5
2hansoul <2hansoul@naver.com>
2023-10-04 07:47:22 +0000

    Feat:멜론top50 크롤링

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
-const express = require('express');
+
+const express = require('express'); //express 모듈 사용
 const app = express();
 const bodyParser = require('body-parser');
+//크롤링사용
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+
+
+
 //const ejs = require('ejs');
 
 //ejs확장자 사용하기 위해 선언
@@ -10,21 +18,22 @@ app.set('views','./views');
 app.use(express.static('public')); //정적인 파일이 이미지
 app.use(bodyParser.urlencoded({extended :false})); // 폼 데이터 post방식 사용위해 미들웨어설정
 
+/*
 //루트페이지 
-app.get('/' ,  function(req,res){
+app.get('/' , function(req,res){
 
   var move_page = `<a href ="/form">form</a>`;
     
   res.send(move_page);
 });
-
+*/
 //url이 form 이면 form.ejs로 이동 
-app.get('/form' ,  function(req,res){
+app.get('/form' , function(req,res){
   res.render('form');
 });
 
 //form 에서 post 방식으로 데이터 전송  body
-app.post('/form_post' ,  function(req,res){
+app.post('/form_post' , function(req,res){
   var title = req.body.title;
   var textare = req.body.textare;
   res.send(title+','+textare);
@@ -32,13 +41,13 @@ app.post('/form_post' ,  function(req,res){
 
 
 //form 에서 get 방식으로 데이터 전송  query
-app.get('/form_get' ,  function(req,res){
-  var title = req.query.title;
+app.get('/form_get' , function(req,res){
+  var title = req.query.title; // 클라이언트에서 입력한 값을 쿼리 형식으로 가져온다  req 는 클라이언트에서 title값은 뭐라고 요청을 했음 
   var textare = req.query.textare;
-  res.send(title);
+  res.send(title+','+textare);
 });
 
-app.get('/topic/:id' ,  function(req,res){
+app.get('/topic/:id' , function(req,res){
   var topics = ['javascript i', 'nodejs', 'express'];
 
   var output =`<a href="topic/0">javascript i</a><br>
@@ -49,7 +58,7 @@ app.get('/topic/:id' ,  function(req,res){
   res.send(output);
 });
 
-app.get ('/topic/:id/:mode' ,function(req,res){
+app.get ('/topic/:id/:mode' , function(req,res){
     res.send(req.params.id+','+req.params.mode);
 });
 
@@ -85,12 +94,58 @@ app.get('/route' ,  function(req,res){
   res.send('hello router <img src="/imgfile.png">');
 });
 
-
-
 app.listen(8080, function(){
+  
   console.log('listening on 8080');
 });
 
 
+/*
+const http = require('http');
+const hostname = '127.0.0.1'; // 로컬 호스트
+const port = 8080;
+const server = http.createServer((req, res) => {
 
+  res.statusCode = 200;
+
+  res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+
+  
+
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server is running at https://${hostname}:${port}/`);
+});
+
+//멜론 크롤링
+const getHtml = async () => {
+  try {
+    // 1
+    const html = await axios.get("https://www.melon.com/chart/index.htm");
+    let ulList = [];
+    // 2
+    const $ = cheerio.load(html.data);
+    // 3
+    const bodyList = $("tr.lst50");
+    bodyList.map((i, element) => {
+      ulList[i] = {
+        rank: i + 1,
+        // 4
+        title: $(element).find("td div.rank01").text().replace(/\s/g, ""),
+        artist: $(element).find("td div.rank02 span").text().replace(/\s/g, ""),
+      };
+    });
+    console.log(ulList);
+    app.get('/' , function(req,res){
+
+      res.send(ulList);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+getHtml();
+*/
 
